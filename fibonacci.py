5664b45f1ed9dd1229bfd244de16451a0d9ffe2b
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 00:07:29 +0900

    Find n-th fibonacci number

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+save = {0:0, 1:1}
+def fibm(n):                # This is the faster methon than the below
+    if not n in save:
+        save[n] = fibm(n-1) + fibm(n-2)
+    return save[n]
+
+def fibonacci(n):
+    if n < 0:
+        return "NO! Please enter the natural number"
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibm(100))
